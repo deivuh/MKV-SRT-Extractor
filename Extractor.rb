cd97c59f006736c6a17346f8a7eb4324aa89c079
David Hsieh <davidhsiehlo@gmail.com>
2014-03-08 14:25:01 -0600

    Adding track shower

---
+++ Extractor.rb
@@ -0,0 +1,4 @@
+
+ARGV.each do|arg|
+
+mkvinfo   ~/Movies/Series/Mobile\ Suit\ Gundam\ 00/Season\ 1/\[OZC\]Mobile\ Suit\ Gundam\ 00\ S1\ Episode\ 01\ \'Celestial\ Being\'\ \[v3\].mkv | grep 'Name\|Track number\|A track'
